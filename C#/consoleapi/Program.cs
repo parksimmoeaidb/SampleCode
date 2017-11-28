dd6da4e7b95f82de70af266cc5a0e979ceaeb51c
led <deded.com.tw@yahoo.com.tw>
2017-11-28 16:03:15 +0800

    Init

---
+++ C#/consoleapi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapi
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var client = new WebClient()) //WebClient  
+            {
+                client.Headers.Add("Content-Type:application/json;charset=utf-8");//Content-Type  
+                client.Headers.Add("Accept:application/json");
+                var result = client.DownloadString("http://210.59.250.98/moeaidbAPI/v1/Company/Company?$top=1&$format=JSON"); //URI
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
